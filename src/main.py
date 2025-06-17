999ec90aea2cd931b41de767d689d0437e8d91df
AspirantDrago <sanekclubstr@mail.ru>
2025-06-17 10:59:05 +0500

    Add main.py

---
+++ src/main.py
@@ -0,0 +1,7 @@
+import os
+import sys
+path = ' '.join(sys.argv[1:])
+for dirpath,dirnames,filenames in os.walk(path):
+    for filename in filenames:
+        print(os.path.join(dirpath, filename))
+
